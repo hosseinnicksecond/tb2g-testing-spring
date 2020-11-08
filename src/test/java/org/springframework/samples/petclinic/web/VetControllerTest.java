edbd6b12f0394a6c3fbf92d9f0c01ba16cff9213
hossein nick <hossinnick62@gmail.com>
2020-11-08 18:18:18 +0330

    add mockMvc and test VetControllerTest in web

--- src/test/java/org/springframework/samples/petclinic/web/VetControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/VetControllerTest.java
@@ -9,18 +9,22 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.samples.petclinic.model.Vet;
 import org.springframework.samples.petclinic.model.Vets;
 import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 class VetControllerTest {
@@ -34,6 +38,8 @@ class VetControllerTest {
     @InjectMocks
     VetController controller;
 
+    MockMvc mockMvc;
+
     List<Vet> vetList= new ArrayList<>();
 
     @BeforeEach
@@ -42,10 +48,20 @@ class VetControllerTest {
         vetList.add(new Vet());
 
         given(service.findVets()).willReturn(vetList);
+
+        mockMvc= MockMvcBuilders.standaloneSetup(controller).build();
     }
 
     @Test
-    void showVetList() {
+    void controllerWebTest() throws Exception {
+        mockMvc.perform(get("/vets.html"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("vets"))
+                .andExpect(view().name("vets/vetList"));
+    }
+
+    @Test
+    void showVetList()  {
 
         String view=controller.showVetList(model);
 
