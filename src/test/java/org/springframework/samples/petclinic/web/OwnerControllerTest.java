02eacc61ebee8db6643b9ac80c90198b3eb872d2
hossein nick <hossinnick62@gmail.com>
2020-11-09 17:57:11 +0330

    test form validation in a post method

--- src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -2,16 +2,32 @@ package org.springframework.samples.petclinic.web;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml","classpath:spring/mvc-core-config.xml"})
+@ExtendWith(MockitoExtension.class)
+@SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml",
+        "classpath:spring/mvc-core-config.xml"})
 class OwnerControllerTest {
 
     @Autowired
@@ -20,11 +36,77 @@ class OwnerControllerTest {
     @Autowired
     ClinicService clinicService;
 
+    @Captor
+    ArgumentCaptor<String> stringArgumentCaptor;
+
+    @Captor
+    ArgumentCaptor<Owner> ownerArgumentCaptor;
+
     MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
         mockMvc= MockMvcBuilders.standaloneSetup(ownerController).build();
+    }
+
+    @Test
+    void findByNameFindOne() throws Exception {
+        List<Owner> owners= new ArrayList<>();
+        Owner owner= new Owner();
+        owner.setId(2);
+//        owner.setLastName("Doe");
+        owners.add(owner);
+        given(clinicService.findOwnerByLastName(anyString())).willReturn(owners);
+        mockMvc.perform(get("/owners")
+                 .param("lastName","Doe"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/2"));
+
+        then(clinicService).should().findOwnerByLastName(stringArgumentCaptor.capture());
+
+        assertThat(stringArgumentCaptor.getValue()).isEqualTo("Doe");
+    }
+
+    @Test
+    void processFormNewOwnerValid() throws Exception {
+        mockMvc.perform(post("/owners/new")
+                  .param("firstName","John")
+                  .param("lastName","Doe")
+                  .param("Address","102 street")
+                  .param("city","newYork")
+                  .param("telephone","121241234"))
+                .andExpect(status().is3xxRedirection());
+
+    }
+
+    @Test
+    void processFormNewOwnerNotValid() throws Exception {
+        mockMvc.perform(post("/owners/new")
+                        .param("firstName","lily")
+                        .param("lastName","Doe")
+                        .param("telephone","11344567"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeHasErrors("owner"))
+                .andExpect(model().attributeHasFieldErrors("owner","address"))
+                .andExpect(model().attributeHasFieldErrors("owner","city"))
+                .andExpect(view().name("owners/createOrUpdateOwnerForm"));
+    }
+
+    @Test
+    void findByNameFindMoreThanOne() throws Exception {
+        List<Owner> owners= new ArrayList<>();
+        owners.add(new Owner());
+        owners.add(new Owner());
+        given(clinicService.findOwnerByLastName("")).willReturn(owners);
+        mockMvc.perform(get("/owners"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("selections"))
+                .andExpect(view().name("owners/ownersList"));
+
+        then(clinicService).should().findOwnerByLastName(stringArgumentCaptor.capture());
+
+        assertThat(stringArgumentCaptor.getValue()).isEqualTo("");
+
     }
 
     @Test
